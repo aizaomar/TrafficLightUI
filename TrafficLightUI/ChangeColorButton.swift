683eb56692cb92f068b33b010678a629354e7e77
aizaomar <w.omaraiza@gmail.com>
2021-11-10 00:37:53 +0600

    ChangeColorButton

---
+++ TrafficLightUI/ChangeColorButton.swift
@@ -0,0 +1,38 @@
+//
+//  ChangeColorButton.swift
+//  TrafficLightUI
+//
+//  Created by Aiza on 10.11.2021.
+//
+
+import SwiftUI
+
+struct ChangeColorButton: View {
+    
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+
+        }
+        .padding()
+        .frame(width: 200, height: 60)
+        .background(Color(.blue))
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .stroke(Color.white, lineWidth: 4)
+        )
+    }
+}
+
+struct ChangeColorButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeColorButton(title: "START", action: {})
+    }
+}
