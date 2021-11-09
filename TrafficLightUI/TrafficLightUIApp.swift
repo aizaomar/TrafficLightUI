e6666753a390f0dd8e59a33355a8b572541f44d9
aizaomar <w.omaraiza@gmail.com>
2021-11-10 00:34:25 +0600

    Initial Commit

---
+++ TrafficLightUI/TrafficLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightUIApp.swift
+//  TrafficLightUI
+//
+//  Created by Aiza on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
